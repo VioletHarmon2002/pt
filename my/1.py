1efc3078b33a5634eed994b39492fbb99c59cec1
elizaveta <liizasabanova@mail.ru>
2023-04-16 22:48:37 +0300

    java html

---
+++ my/1.py
@@ -0,0 +1,10 @@
+#Make a program that prints the following story with the user's name and year embedded in it.
+
+#Sample printout
+#Enter name: Maija
+#Enter year: 1572
+#Maija is a brave knight, born in 1572. One morning Maija woke up to a loud noise: a dragon was approaching the village. Only Maija could save the villagers.
+
+nimi = input("Anna nimi: ")
+vuosi = input("Anna vuosi: ")
+print(nimi + " on urhea ritari, syntynyt " + vuonna + ". Eräänä aamuna " + nimi + " heräsi kovaan meluun: lohikäärme lähestyi kylää. Vain " + nimi + " voisi pelastaa kylän asukkaat.")
